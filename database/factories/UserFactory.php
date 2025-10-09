a163064e1654d54e216796142922d564ee6c9d00
Ralf da Silva <125992274+Ralfzinho@users.noreply.github.com>
2025-10-08 22:15:55 -0300

    Add 'dta_nascimento' field to UserFactory for date of birth generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'dta_nascimento' => $this->faker->date('Y-m-d'), // <-- adicionar esta linha
         ];
     }
 
